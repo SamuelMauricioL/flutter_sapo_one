82ae529e18696ac346ce877a1f6e4c0cd29d5bb3
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2025-04-12 08:58:22 -0500

    feat: init project

---
+++ android/app/src/main/kotlin/com/sapo/one/flutter/sapo/one/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sapo.one.flutter_sapo_one
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_sapo_one/app/app.dart';
+import 'package:flutter_sapo_one/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
